dba69af5e32699d11b749bb8a030d9f691cef739
Dillon Teakell <teakelld@icloud.com>
2023-11-06 17:52:00 -0500

    Initial Commit

---
+++ Advanced-Animations/Advanced_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_AnimationsApp.swift
+//  Advanced-Animations
+//
+//  Created by Dillon Teakell on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
